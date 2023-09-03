3f562ef11fc8f557083f75e665788b7f14022276
Nyateng-Jr <nyatengfrankline@gmail.com>
2023-09-03 23:06:07 +0300

    commit message

---
+++ main.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = "Hi guys, I'm Frank"
+    word_to_be_replaced = input("Enter word to be replaced ")
+    word_replacement = input("Enter word replacement ")
+    print(str.replace(word_to_be_replaced, word_replacement))
+
+replace_word()
